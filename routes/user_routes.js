5d970ddc7e9d0ce9e3902883394282e8f48a0330
sanjiv576 <shresthasanjiv576@gmail.com>
2023-10-03 20:36:39 +0545

    add register routes

---
+++ routes/user_routes.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+
+const router = express.Router();
+
+
+const userController = require('../controllers/user_controller');
+
+
+// for creating an account
+router.route('/register')
+    .get((req, res, next) => res.status(405).json({ error: "GET method is not allowed" }))
+    .delete((req, res, next) => res.status(405).json({ error: "DELETE method is not allowed" }))
+    .put((req, res, next) => res.status(405).json({ error: "PUT method is not allowed" }))
+    .post(userController.userRegister);
+
+module.exports = router;
