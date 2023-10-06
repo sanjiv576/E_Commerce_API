4afc8c8500cb64884c18cc2373f4c9c1eb9b9e13
sanjiv576 <shresthasanjiv576@gmail.com>
2023-10-06 12:12:18 +0545

    add routes for deleting, adding and updating product

--- routes/admin_routes.js
+++ routes/admin_routes.js
@@ -4,11 +4,22 @@ const { verifyAdmin } = require('../middlewares/auth');
 
 const router = express.Router();
 
+// for adding prouct only by admin
 router.route('/product')
     .get((req, res, next) => res.status(405).json({ error: "GET method is not allowed" }))
     .put((req, res, next) => res.status(405).json({ error: 'PUT method is not allowed' }))
     .delete((req, res, next) => res.status(405).json({ error: 'DELETE method is not allowed' }))
     .post(verifyAdmin, adminController.addProduct);
+
+// for updating and deleting a product only by admin
+router.route('/product/:product_id')
+    .get((req, res, next) => res.status(405).json({ error: "GET method is not allowed" }))
+    .post((req, res, next) => res.status(405).json({ error: 'POST method is not allowed' }))
+    .delete(verifyAdmin, adminController.deleteSingleProduct)
+    .put(verifyAdmin, adminController.updateProduct);
+
+
+
 
 
 module.exports = router;
