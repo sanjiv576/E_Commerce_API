bba861f14a29e718ef596b2ec1ab893868a3f774
sanjiv576 <shresthasanjiv576@gmail.com>
2023-10-07 15:34:26 +0545

    routes for getting products

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -23,4 +23,20 @@ router.route('/login')
     .put((req, res, next) => res.status(405).json({ error: "PUT method is not allowed" }))
     .post(userController.userLogin);
 
+
+// get products by admin + registered users + guests
+router.route('/products')
+    .post((req, res, next) => res.status(405).json({ error: "POST method is not allowed" }))
+    .delete((req, res, next) => res.status(405).json({ error: "DELETE method is not allowed" }))
+    .put((req, res, next) => res.status(405).json({ error: "PUT method is not allowed" }))
+    .get(userController.getAllProducts);
+
+
+// for a single product by admin + registered users + guests
+router.route('/:product_id')
+    .post((req, res, next) => res.status(405).json({ error: "POST method is not allowed" }))
+    .delete((req, res, next) => res.status(405).json({ error: "DELETE method is not allowed" }))
+    .put((req, res, next) => res.status(405).json({ error: "PUT method is not allowed" }))
+    .get(userController.getSingleProduct);
+
 module.exports = router;
